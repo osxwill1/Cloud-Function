9a7f56c6cdd1be887b3d3de3925c0eea178d9cb6
Florian Will <osx.will1@gmail.com>
2019-12-01 21:07:50 +0100

    getList

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,17 +5,6 @@ admin.initializeApp()
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const getAvatar = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('films').get()
-    .then(snapshot => {
-        response.send(snapshot.docs.map(doc => doc.data()))
-    })
-    .catch(error => {
-        console.log(error)
-        response.status(500).send(error)
-    } )
-})
-
 export const getList = functions.https.onRequest((request, response) => {
     admin.firestore().collection('films').get()
     .then(snapshot => {
