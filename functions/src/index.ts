aab7aacba502737c142badcd5d0dd223c764df4f
Florian Will <osx.will1@gmail.com>
2019-12-10 20:42:03 +0100

    Fertig

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,48 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 admin.initializeApp()
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+export const getListAF = functions
+.region('europe-west1')
+.https.onCall(async (data, context) => {
+    const films = await admin.firestore().collection('films').get();
+    
+    return films.docs.map(doc => {
+        return {
+            gtin: doc.data().gtin,
+            title: doc.data().title,
+            subtitle: doc.data().subtitle,
+            authors: doc.data().authors,
+            thumbnails: [ doc.data().thumbnails[0] ]
+        }
+    });
+});
 
-export const getList = functions.https.onRequest((request, response) => {
+export const getListHC = functions
+.region('europe-west1')
+.https.onRequest((request, response) => {
+    response.set('Access-Control-Allow-Origin', "*")
+    response.set('Access-Control-Allow-Methods', 'GET')
+    admin.firestore().collection('films').get()
+    .then(snapshot => {
+        response.send(snapshot.docs.map(doc => {
+            return {
+                gtin: doc.data().gtin,
+                title: doc.data().title,
+                subtitle: doc.data().subtitle,
+                authors: doc.data().authors,
+                thumbnails: [ doc.data().thumbnails[0] ]
+            }
+        }))
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(500).send(error)
+    })
+});
+
+export const getListAll = functions.https.onRequest((request, response) => {
+    response.set('Access-Control-Allow-Origin', "*")
+    response.set('Access-Control-Allow-Methods', 'GET')
     admin.firestore().collection('films').get()
     .then(snapshot => {
         response.send(snapshot.docs.map(doc => doc.data()))
@@ -14,4 +52,4 @@ export const getList = functions.https.onRequest((request, response) => {
         console.log(error)
         response.status(500).send(error)
     } )
-})
+});
